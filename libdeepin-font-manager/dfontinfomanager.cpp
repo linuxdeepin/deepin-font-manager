a77bab73583b85b138a439100c256390126aef68
liang.wang <liang.wang@deepin.io>
2019-08-30 11:01:10 +0800

    chore: fix compile error

--- libdeepin-font-manager/dfontinfomanager.cpp
+++ libdeepin-font-manager/dfontinfomanager.cpp
@@ -191,7 +191,6 @@ DFontInfo *DFontInfoManager::getFontInfo(const QString &filePath)
             // }
 
             switch (sname.name_id) {
-<<<<<<< HEAD
             case TT_NAME_ID_COPYRIGHT:
                 fontInfo->copyright = convertToUtf8((char *)sname.string, sname.string_len);
                 fontInfo->copyright = fontInfo->copyright.simplified();
@@ -208,24 +207,6 @@ DFontInfo *DFontInfoManager::getFontInfo(const QString &filePath)
                 break;
             default:
                 break;
-=======
-                case TT_NAME_ID_COPYRIGHT:
-                    fontInfo->copyright = convertToUtf8((char *)sname.string, sname.string_len);
-                    fontInfo->copyright = fontInfo->copyright.simplified();
-                    break;
-
-                case TT_NAME_ID_VERSION_STRING:
-                    fontInfo->version = convertToUtf8((char *)sname.string, sname.string_len);
-                    fontInfo->version = fontInfo->version.remove("Version").simplified();
-                    break;
-
-                case TT_NAME_ID_DESCRIPTION:
-                    fontInfo->description = convertToUtf8((char *)sname.string, sname.string_len);
-                    fontInfo->description = fontInfo->description.simplified();
-                    break;
-                default:
-                    break;
->>>>>>>     chore: add sqlite management, handle font collection and font category filter
             }
         }
     }
