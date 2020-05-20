09f8055395e3353af87baa470357badcbc4c8a38
赵公强 <zhaogongqiang@uniontech.com>
2020-05-20 16:39:31 +0800

    Type:fix Description:fix Occasionally appear the installation process is stuck
    RootCause:A function gets stuck because the incoming parameter is incorrect
    
    Solution:Modify incoming parameters

--- deepin-font-manager/interfaces/dfontpreviewlistdatathread.cpp
+++ deepin-font-manager/interfaces/dfontpreviewlistdatathread.cpp
@@ -242,7 +242,11 @@ int DFontPreviewListDataThread::insertFontItemData(const QString &filePath,
     itemData.fontInfo.isInstalled = true;
 
     /* Bug#16821 UT000591  添加字体后需要加入到Qt的字体数据库中，否则无法使用*/
-    int appFontId = QFontDatabase::addApplicationFont(itemData.fontInfo.filePath);
+//    qDebug() << "1" << itemData.fontInfo.filePath << endl;
+    //ut000442 这里不能直接传路径,需要传字体文件名,传路径会导致偶现这个函数耗时变长
+    int appFontId = QFontDatabase::addApplicationFont(itemData.fontInfo.filePath.split("/").last());
+//    int appFontId = QFontDatabase::addApplicationFont(itemData.fontInfo.filePath);
+//    qDebug() << "2" << endl;
     //中文字体
     if (itemData.fontInfo.isSystemFont && itemData.isChineseFont) {
         QStringList fontFamilyList = QFontDatabase::applicationFontFamilies(appFontId);
@@ -337,13 +341,14 @@ void DFontPreviewListDataThread::refreshFontListData(bool isStartup,  bool isFir
         int maxFontId = m_dbManager->getCurrMaxFontId();
         QList<QString> diffFilePathList = diffSet.toList();
         int index = maxFontId + 1;
-        for (QString &filePath : diffFilePathList) {
+        foreach (QString filePath, diffFilePathList) {
             if (m_dbManager->isSystemFont(filePath) || installFont.contains(filePath)) {
                 bool isEnabled = (isStartup && installFont.contains(filePath)) ? false : true;
+//                qDebug() << __func__ << "s" << endl;
                 index = insertFontItemData(filePath, index, chineseFontPathList, monoSpaceFontPathList, isStartup, isEnabled);
+//                qDebug() << __func__ << "e" << endl;
             }
         }
-
         m_dbManager->commitAddFontInfo();
     }
     if (!isStartup && !installFont.isEmpty()) {
@@ -352,7 +357,6 @@ void DFontPreviewListDataThread::refreshFontListData(bool isStartup,  bool isFir
     } else {
         Q_EMIT m_view->multiItemsAdded(m_fontModelList);
     }
-    qDebug() << __FUNCTION__ << " end";
 }
 
 void DFontPreviewListDataThread::removeFontData(const DFontPreviewItemData &removeItemData)
