895efadc93cee57da569caf3b613c51de573578e
赵公强 <zhaogongqiang@uniontech.com>
2020-06-18 15:43:22 +0800

    fix:bug 33870,Occasionally the installation process cannot end
    Description: Replaced the function that caused the problem
    
    Log: 偶现安装进程无法启动/结束，导致应用无法启动（1/20）
    
    link: https://pms.uniontech.com/zentao/bug-view-33870.html

--- libdeepin-font-manager/dfontmanager.cpp
+++ libdeepin-font-manager/dfontmanager.cpp
@@ -299,13 +299,12 @@ void DFontManager::doInstall(const QStringList &fileList, bool reinstall)
         }
     }
 
-//    QProcess process;
+    QProcess process;
+    process.start("fc-cache");
+//waitforfinished这个函数不设置初始值的话会造成阻塞，设置了初始值之后暂时没有发现什么问题
+    process.waitForFinished(50);
 
-//    process.start("fc-cache");
-//    process.waitForFinished(50);
-
-    //之前使用了waitforfinished这个函数，这个函数造成了没有必要的堵塞，导致后面流程受阻，现在换成下面这个函数，暂时没有发现问题。
-    QProcess::startDetached("fc-cache");
+//    QProcess::startDetached("fc-cache");
 
 //    if (!reinstall) {
 //        QString filename;
